195ae068cb26101e5ee262b9ab7cd08203c8cf16
Tegor International <104412526+Tegorinternational@users.noreply.github.com>
2022-08-19 03:14:30 +0530

    Update storedata.js

--- hr-slide-shop/storedata.js
+++ hr-slide-shop/storedata.js
@@ -3,74 +3,74 @@ const data = [
   {
     id: 1,
     getproductimg:
-      "https://source.unsplash.com/500x500/?transistor",
-    getproductname: "GSM-136 Module",
-    getproductid: "7700",
-    getproductDescription: "It a triple diode based transistor.",
-    getfullprice: "4000",
-    getdiscount: "20",
-    getdiscountedPrice: "10",
-    buyButton: "https://yahoo.com"
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0ile0CGz6ULkgXvbngvqlu-A3W-KjwJODNG4o5FjKuuSOUXmWFlEolbwvupIIu36xggFvWy2fkzBxuMmYvSr2PKOelNv4TWLUh1nkoK5kJk4QlA9MevFHfJLM3vGSxUsyfdRLSCN396JexHdkqd7oNdJHDBagk_QFBqwXxj4SVYgpiBFB0PByKWRU-g/s320/Logo5.png",
+    getproductname: "Vita Gamer Logo design",
+    getproductid: "0120",
+    getproductDescription: "Vita Gamer Logo design for YouTube channel & website ",
+    getfullprice: "1800",
+    getdiscount: "0",
+    getdiscountedPrice: "1800",
+    buyButton: "https://wa.me/918889865223?text=0120"
   },
   {
     id: 1,
     getproductimg:
-      "https://source.unsplash.com/500x500/?boobs",
-    getproductname: "GSM-136 Module",
-    getproductid: "7700",
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjaZVpY-cd7ssqesyFQFsaw05iasR3TEGKdHRGn7HX2VIC35KtaPtNsi71H0tvCP3_x3t-tQU7hylUlZo0tEGuNe3hRIEIwg0AALTIxFcnlKljCyU-6N5fWoBb8KEJKsIqE2T0I5ofENsYa7DhKtqDi11QXAuzXsyhHSuQeQoYjjJKKPZIJ41Acd1M_8g/s320/Logo4.png",
+    getproductname: "AMI builders",
+    getproductid: "0121",
     getproductDescription: "It a triple diode based transistor.",
-    getfullprice: "4000",
-    getdiscount: "20",
-    getdiscountedPrice: "10",
-    buyButton: "https://yahoo.com"
+    getfullprice: "800",
+    getdiscount: "0",
+    getdiscountedPrice: "800",
+    buyButton: "https://wa.me/918889865223?text=0121"
   },
   {
     id: 1,
     getproductimg:
-      "https://source.unsplash.com/500x500/?drinks",
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhM54nh0zSrW7hE8mOktYvJT_OrV-aodrozg8QoZbIoENjdgB8gCSvnchXnC1cyiM4Vp6BPJX80tIkxY02bV0PjvZq9NfAI-GggYH5NmlR5laW4RxapofxR5FYjKaRZ9CTPsPky6VK3pRUJymDX_95pSgLHDp0dlnafL5Ye0APRvYNgsp3K8EBVS2K2Dg/s320/L19.png",
     getproductname: "BT-136 Module",
-    getproductid: "7700",
+    getproductid: "0122",
     getproductDescription: "It a triple diode based transistor.",
-    getfullprice: "4000",
-    getdiscount: "20",
-    getdiscountedPrice: "10",
-    buyButton: "https://yahoo.com"
+    getfullprice: "1180",
+    getdiscount: "0",
+    getdiscountedPrice: "1180",
+    buyButton: "https://wa.me/918889865223?text=0122"
   },
   {
     id: 1,
     getproductimg:
-      "https://source.unsplash.com/500x500/?radio",
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEihLN2IevCJAAbo3Qi6buuuQXLJ9hMONIlI5VC_Pb-gg3lTMlGN-8JiaFJcltneFKwY-bpqCZO6UASDtGH_HtzexHMiyyJs5GzWfGwG7rAQSc8BaJyedy8ZNamUdq3O2mF-sk_4rg8FXy4vCdI_rnkH-SlgzvSVOitMNYR79UC6u8bJmzaa-U6YWzabKg/s320/IMG_20220819_024008.jpg",
     getproductname: "BT-136 Module",
-    getproductid: "7700",
+    getproductid: "0123",
     getproductDescription: "It a triple diode based transistor.",
-    getfullprice: "4000",
-    getdiscount: "20",
-    getdiscountedPrice: "10",
-    buyButton: "https://yahoo.com"
+    getfullprice: "580",
+    getdiscount: "0",
+    getdiscountedPrice: "580",
+    buyButton: "https://wa.me/918889865223?text=0123"
   },
   {
     id: 1,
     getproductimg:
-      "https://source.unsplash.com/500x500/?zam",
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEii-Vad1UzUExu-dshVC7ZF0omiAoTxFa67CIIoQ9dOd8LgVpCZZIMiMe72is9K_Me0TKHOXBi4DJNb9hQYeSH62dGDwolI3ymaCp7H30m9TAbJ0aK3aCzeQ6HRuF0BtUckBmOhmJ0-vDqhC-SfkOx3U9tQRdqcd4la1pSlTO0hNDhqRA12WQy_eqK_4Q/s320/IMG_20220819_023752.png",
     getproductname: "BT-136 Module",
-    getproductid: "7700",
+    getproductid: "0124",
     getproductDescription: "It a triple diode based transistor.",
-    getfullprice: "4000",
-    getdiscount: "20",
-    getdiscountedPrice: "10",
-    buyButton: "https://yahoo.com"
+    getfullprice: "499",
+    getdiscount: "0",
+    getdiscountedPrice: "499",
+    buyButton: "https://wa.me/918889865223?text=0124"
   },
   {
     id: 1,
     getproductimg:
-      "https://source.unsplash.com/500x500/?biscuits",
-    getproductname: "RESM-136 Module",
-    getproductid: "720",
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj11aLU9h58dfapSJ0jWW5xpiRGykJS0bHlvDf_K59R_BtmokO7g2RCp3dDKp8FmXTrWbrl6Uph-z9a67rtobfyRI0awYG_LCMWnRP2GH59JQFNK6dkZEZijTkdfGhqIw86tXK8NbJDrB3nm_7URL6Fei3opIkwXJyiyjkW5XLMHeCV_R4R2uMq3feyqw/s320/IMG_20220728_093035.jpg",
+    getproductname: "IEES",
+    getproductid: "0125",
     getproductDescription: "one h a triple diode based transistor.",
-    getfullprice: "4000",
-    getdiscount: "20",
-    getdiscountedPrice: "10",
-    buyButton: "https://yahoo.com"
+    getfullprice: "700",
+    getdiscount: "0",
+    getdiscountedPrice: "700",
+    buyButton: "https://wa.me/918889865223?text=0125"
   }
   
 ];
